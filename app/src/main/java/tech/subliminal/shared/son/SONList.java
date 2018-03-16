98e4e8e3524e6ef7921347673469769b109dec4d
de Graaff Rik - graaff <rik.degraaff@unibas.ch>
2018-03-16 17:54:45 +0100

    defined interface of SONList. #2, still needs comments

--- app/src/main/java/tech/subliminal/shared/son/SONList.java
+++ app/src/main/java/tech/subliminal/shared/son/SONList.java
@@ -1,4 +1,66 @@
 package tech.subliminal.shared.son;
 
+import java.util.Optional;
+
 public class SONList {
+
+    public SONList set(int value, int i) {
+
+        return this;
+    }
+
+    public SONList set(double value, int i) {
+
+        return this;
+    }
+
+    public SONList set(boolean value, int i) {
+
+        return this;
+    }
+
+    public SONList set(String value, int i) {
+
+        return this;
+    }
+
+    public SONList set(SON value, int i) {
+
+        return this;
+    }
+
+    public SONList set(SONList value, int i) {
+
+        return this;
+    }
+
+    public Optional<Integer> getInt(int i) {
+
+        return Optional.empty();
+    }
+
+    public Optional<Double> getDouble(int i) {
+
+        return Optional.empty();
+    }
+
+    public Optional<Boolean> getBoolean(int i) {
+
+        return Optional.empty();
+    }
+
+    public Optional<String> getString(int i) {
+
+        return Optional.empty();
+    }
+
+    public Optional<SON> getObject(int i) {
+
+        return Optional.empty();
+    }
+
+    public Optional<SONList> getList(int i) {
+
+        return Optional.empty();
+    }
 }
