aa58ffa172e01882766167ee66f4181c5e32e4ff
David Lengweiler - lendav00 <david.lengweiler@stud.unibas.ch>
2018-03-18 15:06:12 +0100

    added ClientInitializer. partial #6

---
+++ app/src/main/java/tech/subluminal/client/init/ClientInitializer.java
@@ -0,0 +1,28 @@
+package tech.subluminal.client.init;
+
+import java.net.InetAddress;
+import tech.subluminal.client.logic.UserManager;
+import tech.subluminal.shared.net.Connection;
+import tech.subluminal.tech.subluminal.client.stores.InMemoryUserStore;
+import tech.subluminal.tech.subluminal.client.stores.UserStore;
+
+/**
+ * Assembles the client-side architecture.
+ */
+public class ClientInitializer {
+
+  /**
+   * Initializes the assembling.
+   *
+   * @param server address of the server(hostname or ip).
+   * @param port of the server.
+   * @param username initial username to request from the server.
+   */
+  public static void init(String server, int port, String username) {
+    Connection connection = null; // new ClientSocketConneciton(server, port);
+    UserStore userStore = new InMemoryUserStore();
+    UserManager userManager = new UserManager(connection, userStore);
+    
+    userManager.start(username);
+  }
+}
