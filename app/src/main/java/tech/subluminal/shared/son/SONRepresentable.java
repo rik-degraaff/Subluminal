5729b65ef5d567735db8c504fa6f025e2ef3286b
de Graaff Rik - graaff <rik.degraaff@unibas.ch>
2018-03-18 06:37:02 +0100

    Defined the SONRepresentable interface, closes #5

---
+++ app/src/main/java/tech/subluminal/shared/son/SONRepresentable.java
@@ -0,0 +1,16 @@
+package tech.subluminal.shared.son;
+
+/**
+ * An interface for java objects that can be represented as a SON object.
+ * Java does not allow interfaces to declare static methods for child classes to implement,
+ * it is highly recommended that you implement a `fromSON(SON)` static method when implementing this interface.
+ */
+public interface SONRepresentable {
+    /**
+     * Creates a SON object representing this object.
+     *
+     * @return the SON representation.
+     */
+    SON asSON();
+    // static Self fromSON(SON son);
+}
