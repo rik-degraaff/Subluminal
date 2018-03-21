a7e74739ed807445546f6583d7d5005c0563e132
David Lengweiler - lendav00 <david.lengweiler@stud.unibas.ch>
2018-03-21 18:50:51 +0100

    created ChatPresenter object

--- app/src/main/java/tech/subluminal/client/presentation/ChatPresenter.java
+++ app/src/main/java/tech/subluminal/client/presentation/ChatPresenter.java
@@ -1,6 +1,7 @@
 package tech.subluminal.client.presentation;
 
 import java.util.Optional;
+import tech.subluminal.shared.son.SONRepresentable;
 
 /**
  * Handles the presentation of the chat.
@@ -8,18 +9,45 @@ import java.util.Optional;
 public interface ChatPresenter {
 
   /**
-   * Gets called when the User wants to send a new message.
+   * Gets called when the User wants to send a new message to everyone.
+   *
    * @param message is the text to be send.
-   * @param type defines for who the message is.
-   * @param receiver if the message is private. /TODO make optional
    */
-  void sendMessage( String type, String message, String receiver);
+  void sendGlobalMessage(String message);
 
   /**
+   * Send a message to all players in the same game.
+   */
+  void sendGameMessage(String message);
+
+  /**
+   * Send a message to a single player.
+   *
+   * @param message The actual message.
+   */
+  void sendWhisperMessage(String message, String username);
+
+  /**
+   * Fired when a someone sends a message to all users on the server.
    *
    * @param username from the sender of the message.
    * @param message is the text of the message.
-   * @param type specifies the type of the message.
    */
-  void receiveMessage(String message, String type, String username);
+  void GlobalMessageReceived(String message, String username);
+
+  /**
+   * Fired when a personal message is received.
+   *
+   * @param message of the received whisper.
+   * @param username of the sender.
+   */
+  void WhisperMessageReceived(String message, String username);
+
+  /**
+   * Fired when a message from the same game is received.
+   *
+   * @param message of the game message.
+   * @param username fo the sender.
+   */
+  void GameMessageReceived(String message, String username);
 }
