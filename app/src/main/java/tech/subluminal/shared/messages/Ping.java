ce6f08065b865aa4773c1ce53951224585389c7f
de Graaff Rik - graaff <rik.degraaff@unibas.ch>
2018-03-22 10:41:53 +0100

    Created Ping and Pong Messages. initial #17

---
+++ app/src/main/java/tech/subluminal/shared/messages/Ping.java
@@ -0,0 +1,47 @@
+package tech.subluminal.shared.messages;
+
+import tech.subluminal.shared.son.SON;
+import tech.subluminal.shared.son.SONConversionError;
+import tech.subluminal.shared.son.SONRepresentable;
+
+/**
+ * Represents a ping from client to server or vice versa.
+ */
+public class Ping implements SONRepresentable {
+
+  private static final String ID_KEY = "id";
+  private String id;
+
+  /**
+   * Creates a ping with a given id.
+   *
+   * @param id the id of the ping.
+   */
+  public Ping(String id) {
+    this.id = id;
+  }
+
+  /**
+   * Creates a SON object representing this object.
+   *
+   * @return the SON representation.
+   */
+  @Override
+  public SON asSON() {
+    return new SON().put(id, ID_KEY);
+  }
+
+  /**
+   * Creates a ping from a SON object.
+   *
+   * @param son the SON object to be converted to a ping.
+   * @return the created ping.
+   * @throws SONConversionError when the conversion fails.
+   */
+  public static Ping fromSON(SON son) throws SONConversionError {
+    String id = son.getString(ID_KEY)
+        .orElseThrow(() -> new SONConversionError(
+            "Ping did not contain valid " + ID_KEY + "."));
+    return new Ping(id);
+  }
+}
