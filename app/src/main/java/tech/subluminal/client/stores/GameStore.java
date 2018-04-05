a8297dd4d759b6e2fb4fd95ee0962936f297d012
Galery Käser Lucas - galluc00 <l.galerykaeser@stud.unibas.ch>
2018-04-05 14:42:23 +0200

    Adapted the Synchronized functionality to the GameStore.

--- app/src/main/java/tech/subluminal/client/stores/GameStore.java
+++ app/src/main/java/tech/subluminal/client/stores/GameStore.java
@@ -1,5 +1,6 @@
 package tech.subluminal.client.stores;
 
+import java.util.Collection;
 import java.util.Optional;
 import tech.subluminal.client.stores.records.game.Player;
 import tech.subluminal.shared.stores.records.game.Star;
@@ -7,14 +8,15 @@ import tech.subluminal.shared.util.Synchronized;
 
 public interface GameStore {
 
+  Synchronized<Collection<Synchronized<Star>>> getStars();
+
   Optional<Synchronized<Star>> getStarByID(String id);
 
   void addStar(Star star);
 
+  Synchronized<Collection<Synchronized<Player>>> getPlayers();
+
+  Optional<Synchronized<Player>> getPlayerByID(String id);
+
   void addPlayer(Player player);
-
-  Optional<Synchronized<Player>> getPlayerByID();
-
-
-
 }
---
+++ app/src/main/java/tech/subluminal/client/stores/InMemoryGameStore.java
@@ -0,0 +1,43 @@
+package tech.subluminal.client.stores;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import tech.subluminal.client.stores.records.game.Player;
+import tech.subluminal.shared.stores.records.game.Star;
+import tech.subluminal.shared.util.Synchronized;
+
+public class InMemoryGameStore implements GameStore {
+  private Synchronized<Map<String, Synchronized<Star>>> starMap;
+  private Synchronized<Map<String, Synchronized<Player>>> playerMap;
+
+  @Override
+  public Synchronized<Collection<Synchronized<Star>>> getStars() {
+    return starMap.map(Map::values);
+  }
+
+  @Override
+  public Optional<Synchronized<Star>> getStarByID(String id) {
+    return Optional.ofNullable(starMap.use(map -> map.get(id)));
+  }
+
+  @Override
+  public void addStar(Star star) {
+    starMap.use(map -> map.put(star.getId(), new Synchronized<>(star)));
+  }
+
+  @Override
+  public Synchronized<Collection<Synchronized<Player>>> getPlayers() {
+    return playerMap.map(Map::values);
+  }
+
+  @Override
+  public Optional<Synchronized<Player>> getPlayerByID(String id) {
+    return Optional.ofNullable(playerMap.use(map -> map.get(id)));
+  }
+
+  @Override
+  public void addPlayer(Player player) {
+    playerMap.use(map -> map.put(player.getId(), new Synchronized<>(player)))
+  }
+}
