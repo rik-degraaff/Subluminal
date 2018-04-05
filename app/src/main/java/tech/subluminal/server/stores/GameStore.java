6449d1c61739a5974e4e63db4d77299dadd45233
de Graaff Rik - graaff <rik.degraaff@unibas.ch>
2018-04-05 10:43:29 +0200

    Started defining client side GameStore interface

--- app/src/main/java/tech/subluminal/server/stores/GameStore.java
+++ app/src/main/java/tech/subluminal/server/stores/GameStore.java
@@ -1,5 +1,15 @@
 package tech.subluminal.server.stores;
 
+import java.util.Optional;
+import tech.subluminal.shared.records.game.Star;
+
 public interface GameStore {
 
+  Optional<Star> getStarByID(String id);
+
+  void addStar(Star star);
+
+  void updateStar(Star star);
+
+  
 }
