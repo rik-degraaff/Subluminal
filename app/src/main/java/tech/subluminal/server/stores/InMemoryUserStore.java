d27bc2b3193697559a38ea591dad5a0ffbc4fe74
de Graaff Rik - graaff <rik.degraaff@unibas.ch>
2018-04-06 17:32:09 +0200

    Fixed minor formatting errors.

--- app/src/main/java/tech/subluminal/server/stores/InMemoryUserStore.java
+++ app/src/main/java/tech/subluminal/server/stores/InMemoryUserStore.java
@@ -1,12 +1,7 @@
 package tech.subluminal.server.stores;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 import tech.subluminal.shared.stores.IdentifiableCollection;
 import tech.subluminal.shared.stores.records.User;
-import tech.subluminal.shared.util.Synchronized;
 
 /**
  * Stores server-side information about the users in memory.
