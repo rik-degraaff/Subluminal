cc06e92858f7201d2a717cc45216dd1a77a2a2c6
David Lengweiler lendav00 <david.lengweiler@stud.unibas.ch>
2018-05-02 13:43:18 +0200

    characters get printed correctly if Dflag is set

--- app/src/main/java/tech/subluminal/shared/util/NameGenerator.java
+++ app/src/main/java/tech/subluminal/shared/util/NameGenerator.java
@@ -1,15 +1,15 @@
 package tech.subluminal.shared.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.regex.Pattern;
-import org.pmw.tinylog.Logger;
-import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
 
 public class NameGenerator {
 
@@ -17,62 +17,61 @@ public class NameGenerator {
   private List<String> planetNames; //for later use
 
   public NameGenerator() {
+
     readStarFiles();
+
   }
 
   public void readStarFiles() {
-    Reflections reflections = new Reflections("tech.subluminal", new ResourcesScanner());
-    Set<String> fileNames = reflections.getResources(Pattern.compile(".*\\.csv"));
+    File folder = new File("/tech/subluminal/resources/namegenerator/stars");
+    File[] listOfFiles = folder.listFiles();
+    //Logger.debug(listOfFiles.toString());
 
-    if (fileNames.isEmpty()) {
-      throw new IllegalArgumentException("No list with starnames in resources found!");
-    } else {
-      Logger.info(".csv files found:");
-      fileNames.forEach(file -> {
-        Logger.info(file);
-        readLines(file);
-      });
-    }
+    /*for (int i = 0; i < listOfFiles.length; i++) {
+      if (listOfFiles[i].isFile()) {
+        String file = listOfFiles[i].getName();
+        System.out.println("File " + file);
+        // get last index for '.' char
+        int lastIndex = file.lastIndexOf('.');
+
+        // get extension
+        String str = file.substring(lastIndex);
+
+        // match path name extension
+        if (str.equals(".txt")) {
+          readLines("/tech/subluminal/resources/namegenerator/stars/" + listOfFiles[i].getName());
+        }
+
+      }
+    }*/
+    //readLines("/tech/subluminal/resources/namegenerator/stars/nearest-stars-single.txt");
   }
 
   private void readLines(String path) {
-//    File fileDir = new File("D:\\_projects\\Gruppe-11\\assets\\other\\starlist\\nearest-stars.csv");
-//
-//    try {
-//      BufferedReader in = new BufferedReader(
-//          new InputStreamReader(
-//              new FileInputStream(fileDir)));
-//
-//      String str;
-//
-//      while ((str = in.readLine()) != null) {
-//        System.out.println(str);
-//      }
-//
-//      //System.out.println(System.getProperty("file.encoding"));
-//      in.close();
-//
-//    } catch (UnsupportedEncodingException e) {
-//      e.printStackTrace();
-//    } catch (FileNotFoundException e) {
-//      e.printStackTrace();
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
-
-    Scanner sc = new Scanner(NameGenerator.class.getResourceAsStream("/" + path), "UTF-8");
-    while (sc.hasNext()) {
-      String line = sc.nextLine();
-      //System.out.println(line);
-      starNames.add(line.split(";")[0]);
+    System.out.println(NameGenerator.class.getResource(path).getPath());
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new InputStreamReader(
+          NameGenerator.class.getResource(path).openStream(), "UTF-8"));
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    sc.close();
+
+    br.lines().forEach(l -> {
+      starNames.add(l);
+      System.out.println(l);
+    });
+
   }
 
   public String getName() {
     Random rand = new Random();
-    int rIndex = rand.nextInt(starNames.size()-1);
-    return starNames.remove(rIndex+1);
+    int rIndex = rand.nextInt(starNames.size() - 1);
+    return starNames.remove(rIndex + 1);
   }
 
   public List<String> getNames(int count) {
@@ -80,11 +79,11 @@ public class NameGenerator {
     List<String> names = new LinkedList<>();
     int rIndex;
     for (int i = 0; i < count; i++) {
-      rIndex = rand.nextInt(starNames.size()-1);
-      if (names.contains(planetNames.get(rIndex+1))) {
+      rIndex = rand.nextInt(starNames.size() - 1);
+      if (names.contains(planetNames.get(rIndex + 1))) {
         i--;
       } else {
-        names.add(planetNames.remove(rIndex+1));
+        names.add(planetNames.remove(rIndex + 1));
       }
     }
     return names;
@@ -93,7 +92,7 @@ public class NameGenerator {
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    starNames.forEach(s -> sb.append(s+"\n"));
+    starNames.forEach(s -> sb.append(s + "\n"));
 
     return sb.toString();
   }
