c87327a0389b260fbdc7df2d7d05d2f680fac869
Galery Käser Lucas - galluc00 <l.galerykaeser@stud.unibas.ch>
2018-05-03 17:07:47 +0200

    Added InitialUsersTest and LobbyListResTest.

---
+++ app/src/test/java/tech/subluminal/shared/messages/InitialUsersTest.java
@@ -0,0 +1,56 @@
+package tech.subluminal.shared.messages;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Test;
+import tech.subluminal.shared.son.SON;
+import tech.subluminal.shared.son.SONConversionError;
+import tech.subluminal.shared.son.SONParsingError;
+import tech.subluminal.shared.stores.records.User;
+
+public class InitialUsersTest {
+
+  @Test
+  public void testStringifyAndParsing() {
+    User user1 = new User("sölkdfa", "2917");
+    User user2 = new User("asdöflkj", "049851");
+    InitialUsers iu = new InitialUsers();
+    iu.addUser(user1);
+    iu.addUser(user2);
+    List<User> users = iu.getUsers();
+    String iuStringified = iu.asSON().asString();
+    InitialUsers parsedIU = null;
+    List<User> parsedUsers = null;
+    try {
+      parsedIU = InitialUsers.fromSON(SON.parse(iuStringified));
+      parsedUsers = parsedIU.getUsers();
+    } catch (SONConversionError | SONParsingError e) {
+      e.printStackTrace();
+    }
+
+    boolean containsName = false;
+    boolean scoreIsRight = false;
+
+
+    for (int i = 0; i < users.size(); i++) {
+      containsName = false;
+      scoreIsRight = false;
+      for (int j = 0; j < parsedUsers.size(); j++) {
+        if (users.get(i).getUsername().equals(parsedUsers.get(j).getUsername())) {
+          containsName = true;
+          if (users.get(i).getID().equals(parsedUsers.get(j).getID())) {
+            scoreIsRight = true;
+          }
+        }
+      }
+    }
+
+    assertTrue(containsName);
+    assertTrue(scoreIsRight);
+    assertEquals(users.size(), parsedUsers.size());
+  }
+}
