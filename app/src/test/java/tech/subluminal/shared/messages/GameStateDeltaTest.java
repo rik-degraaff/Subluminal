5fe034d709ab0596ea48898c33b8a8a259c11cd2
de Graaff Rik - graaff <rik.degraaff@unibas.ch>
2018-05-03 18:46:25 +0200

    Modified tests for GameStateDelta to account for MotherSHips being optional.

--- app/src/test/java/tech/subluminal/shared/messages/GameStateDeltaTest.java
+++ app/src/test/java/tech/subluminal/shared/messages/GameStateDeltaTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 import org.junit.Test;
 import tech.subluminal.client.stores.records.game.Player;
 import tech.subluminal.shared.records.LobbyStatus;
@@ -23,7 +24,8 @@ public class GameStateDeltaTest {
   public void stringifyAndParseGameStateDelta() {
     GameStateDelta delta = new GameStateDelta();
     delta.addPlayer(new Player("1234",
-        new Ship(new Coordinates(1.0, 2.3), "4321", Arrays.asList("1", "2", "3"), "3", 0.2),
+        Optional.of(new Ship(new Coordinates(1.0, 2.3), "4321", Arrays.asList("1", "2", "3"), "3",
+            0.2)),
         Arrays.asList(
             new Fleet(new Coordinates(1.0, 2.3), 17, "4321321", Arrays.asList("2", "3"), "3", 0.2),
             new Fleet(new Coordinates(0.0, 2000.13), 17, "4qd321", Arrays.asList("1", "2", "3"),
@@ -34,11 +36,15 @@ public class GameStateDeltaTest {
         )));
 
     delta.addPlayer(new Player("2345",
-        new Ship(new Coordinates(10.0, 2.3), "4321", Collections.emptyList(), "5", 0.2),
+        Optional
+            .of(new Ship(new Coordinates(10.0, 2.3), "4321", Collections.emptyList(), "5", 0.2)),
         Collections.emptyList()));
 
+    delta.addPlayer(new Player("3456", Optional.empty(), Collections.emptyList()));
+
     delta.addStar(new Star("1234", 1, new Coordinates(0, 0), "starid", true, 0.1, "Twinkle"));
-    delta.addStar(new Star(null, 0, new Coordinates(0, 42), "starid2", false, 0.3, "TwinkleTwinkle"));
+    delta.addStar(
+        new Star(null, 0, new Coordinates(0, 42), "starid2", false, 0.3, "TwinkleTwinkle"));
 
     delta.addRemovedFleet("1234", "645");
     delta.addRemovedFleet("1234", "123");
