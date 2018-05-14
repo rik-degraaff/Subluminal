191ec7358ec04897582646bd4805fdf5d76b98bc
Galery Käser Lucas - galluc00 <l.galerykaeser@stud.unibas.ch>
2018-05-14 17:34:30 +0200

    Added string representations to the mentioned messages and removed unnessessary try/catch blocks.

--- app/src/main/java/tech/subluminal/shared/messages/HighScoreReq.java
+++ app/src/main/java/tech/subluminal/shared/messages/HighScoreReq.java
@@ -4,6 +4,13 @@ import tech.subluminal.shared.son.SON;
 import tech.subluminal.shared.son.SONConversionError;
 import tech.subluminal.shared.son.SONRepresentable;
 
+/**
+ * Represents a request message for the highscore. A HighScoreReq message converted to SON and then to string looks like this:
+ * <pre>
+ * {
+ * }
+ * </pre>
+ */
 public class HighScoreReq implements SONRepresentable {
 
   public static HighScoreReq fromSON(SON son) throws SONConversionError {
