88052e49a9b77e62ed8ae7d5e7ad2df63136bf77
Luc Kury - kurluc00 <luc.kury@unibas.ch>
2018-05-14 10:53:56 +0200

    Docker image is invalid, changed ENTRYPOINT to non array form
    https://stackoverflow.com/a/47643816

--- docker/_deploy.sh
+++ docker/_deploy.sh
@@ -26,3 +26,13 @@ apk add --no-cache openssh-client sshpass
 sshpass -p $DEPLOY_PASSWORD ssh -o StrictHostKeyChecking=no $SSH_USER@$DEPLOYHOST docker stop subluminal_$CI_JOB_NAME || true
 sshpass -p $DEPLOY_PASSWORD ssh $SSH_USER@$DEPLOYHOST docker pull $DOCKER_REPO/$DOCKER_IMAGE:latest
 sshpass -p $DEPLOY_PASSWORD ssh $SSH_USER@$DEPLOYHOST docker run --rm -d -p $PORT:1729/tcp --name subluminal_$CI_JOB_NAME $DOCKER_REPO/$DOCKER_IMAGE:latest
+
+## Better version from gitlab-ci.yml
+- docker build -t subluminalthegame/subluminal:latest -t subluminalthegame/subluminal:$(echo $VERSION) --file ./docker/dockerfile-subluminal.yml --build-arg VERSION=$(echo $VERSION) --build-arg PORT=1729 .
+- docker login -u subluminalthegame -p $DOCKERHUB_PASSWORD
+- docker push subluminalthegame/subluminal
+- apk update
+- apk add --no-cache openssh-client sshpass
+- sshpass -p $DEPLOY_PASSWORD ssh -o StrictHostKeyChecking=no gitlab@$DEPLOYHOST docker stop subluminal_staging || true
+- sshpass -p $DEPLOY_PASSWORD ssh gitlab@$DEPLOYHOST docker pull subluminalthegame/subluminal:$VERSION
+- sshpass -p $DEPLOY_PASSWORD ssh gitlab@$DEPLOYHOST docker run --rm -d -p 1727:1729/tcp --label subluminal --name subluminal_$CI_JOB_NAME subluminalthegame/subluminal:$VERSION
