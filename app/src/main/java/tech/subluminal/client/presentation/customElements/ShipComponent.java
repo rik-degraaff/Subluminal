3b6aa04791fdc189a028121c530831449e60e1a9
David Lengweiler lendav00 <david.lengweiler@stud.unibas.ch>
2018-05-21 13:06:00 +0200

    added new fleet pictures and uses new gifs now

--- app/src/main/java/tech/subluminal/client/presentation/customElements/ShipComponent.java
+++ app/src/main/java/tech/subluminal/client/presentation/customElements/ShipComponent.java
@@ -83,11 +83,14 @@ public abstract class ShipComponent extends Pane {
 
     ImageView shipBody = initShipImage("/tech/subluminal/resources/100w/shipBody.png");
     setShipColor(shipBody);
+    shipBody.setPreserveRatio(true);
 
-    ImageView shipDetails = initShipImage("/tech/subluminal/resources/100w/shipDetails.png");
+    ImageView shipDetails = initShipImage("/tech/subluminal/resources/100w/shipDetails.gif");
+    shipDetails.setPreserveRatio(true);
 
     ship.getChildren().addAll(shipBody, shipDetails);
     ship.setMouseTransparent(true);
+
     //ship.prefWidth(SHIP_HEIGHT);
     //ship.prefHeight(SHIP_HEIGHT);
 
